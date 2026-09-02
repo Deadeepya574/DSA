fd816d28744b08fd2b5f6ef35bad228aa0002877
Deadeepya574 <deadeepya574@gmail.com>
2026-09-02 16:54:41 +0530

    Time: 3 ms (45.23%) | Memory: 9.8 MB (25.94%) - LeetSync

---
+++ DSA/242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        vector<int> vec(26,0);
+        for(int i =0 ;i<s.size();i++){
+            vec[s[i]-'a']++;
+            vec[t[i]-'a']--;
+        }
+        for(int x: vec ){
+            if(x!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
